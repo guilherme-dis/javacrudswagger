004fd9688d9468f5cb3cf8b88a598883d91a98e3
Guilherme Dias Cardoso <guilherme.dis@ufu.br>
2022-01-07 21:36:45 -0300

    employee validation

--- src/main/java/com/ufu/javacrudswagger/entities/Provider.java
+++ src/main/java/com/ufu/javacrudswagger/entities/Provider.java
@@ -3,6 +3,8 @@ package com.ufu.javacrudswagger.entities;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
@@ -16,6 +18,8 @@ public class Provider {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(length = 50)
+    @NotEmpty(message = "The Provider name cannot be empty")
+    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
     private String name;
 
     @ManyToOne
