9d028d28c9432272d7071c5d21b578c90fb10468
Guilherme Dias Cardoso <guilherme.dis@ufu.br>
2022-01-07 21:35:35 -0300

    employee validation

--- src/main/java/com/ufu/javacrudswagger/entities/Employee.java
+++ src/main/java/com/ufu/javacrudswagger/entities/Employee.java
@@ -3,6 +3,8 @@ package com.ufu.javacrudswagger.entities;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
@@ -16,12 +18,23 @@ public class Employee {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(length = 50)
+    @NotEmpty(message = "The Employee name cannot be empty")
+    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
     private String name;
+
     @Column(length = 50)
+    @NotEmpty(message = "The Employee occupation cannot be empty")
+    @Size(min = 3, max = 50, message = "Occupation must be between 3 and 50 characters")
     private String occupation;
+
     @Column(length = 50)
+    @NotEmpty(message = "The Employee username cannot be empty")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
     private String username;
+
     @Column(length = 50)
+    @NotEmpty(message = "The Employee password cannot be empty")
+    @Size(min = 3, max = 50, message = "Password must be between 3 and 50 characters")
     private String password;
 
     @ManyToOne
