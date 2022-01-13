3678c0394c1d2f15426c1a0b67e87058b9229baf
Guilherme Dias Cardoso <guilherme.dis@ufu.br>
2022-01-13 13:18:18 -0300

    Releasing swagger access

--- src/main/java/com/ufu/javacrudswagger/security/JWTConfiguracao.java
+++ src/main/java/com/ufu/javacrudswagger/security/JWTConfiguracao.java
@@ -2,7 +2,6 @@ package com.ufu.javacrudswagger.security;
 
 import com.ufu.javacrudswagger.services.DetalheUsuarioServiceImpl;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -29,8 +28,8 @@ public class JWTConfiguracao extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
-                .antMatchers(HttpMethod.POST,"/login").permitAll()
-                .anyRequest().authenticated()
+                .antMatchers("/api/**").authenticated()
+                .anyRequest().permitAll()
                 .and()
                 .addFilter(new JWTAutenticarFilter(authenticationManager()))
                 .addFilter(new JWTValidarFilter(authenticationManager()))
